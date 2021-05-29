0b8a0a8c609cb00e300ce92a44370074a805b6aa
ibrahimuludag <ibrahimuludag@gmail.com>
2021-05-29 16:25:32 +0300

    Move code to partial classes

---
+++ src/WebUI/Pages/Authentication/Authentication.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RapidBlazor.WebUI.Pages.Authentication
+{
+    public partial class Authentication
+    {
+        [Parameter] 
+        public string Action { get; set; }
+
+    }
+}
---
+++ src/WebUI/Pages/Samples/ApplicationInfo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Threading.Tasks;
+
+namespace RapidBlazor.WebUI.Pages.Samples
+{
+    public partial class ApplicationInfo
+    {
+        [Inject]
+        public IAccessTokenProvider AccessTokenProvider { get; set; }
+
+        public string AccessToken { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            // Move this to a service
+            var tokenResult = await AccessTokenProvider.RequestAccessToken();
+
+            if (tokenResult.TryGetToken(out var token))
+            {
+                AccessToken = token.Value;
+            }
+            await base.OnInitializedAsync();
+        }
+    }
+}
