a9a46d8371403d6ef8e074a95f2dc26c4fd8472f
ibrahimuludag <ibrahimuludag@gmail.com>
2021-07-04 21:09:20 +0300

    Fix deployment script

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -7,7 +7,7 @@ Write-Host "Running Idp"
 Start-Process -FilePath "dotnet" -ArgumentList "run" -WorkingDirectory "..\external\Idp\"
 
 Write-Host "Running Api"
-Start-Process -FilePath "dotnet" -ArgumentList "run" -WorkingDirectory "..\src\Api\"
+Start-Process -FilePath "dotnet" -ArgumentList "run" -WorkingDirectory "..\src\RapidBlazor.Api\"
 
 Write-Host "Running WebUI"
-Start-Process -FilePath "dotnet" -ArgumentList "watch run" -WorkingDirectory "..\src\WebUI\"
+Start-Process -FilePath "dotnet" -ArgumentList "watch run" -WorkingDirectory "..\src\RapidBlazor.WebUI\"
